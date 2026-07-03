7d0fc87714aa748e5f2d2b17cc00f1b7ba68ce48
Hash-D25 <harshita2386@gmail.com>
2026-07-03 19:22:04 +0530

    making a schema PostCreate with pydantic, making createpost route

--- app/app.py
+++ app/app.py
@@ -1,7 +1,28 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,HTTPException
+from app.schemas import PostCreate
 
 app=FastAPI()
 
-@app.get("/")
-def root():
-    return {"message":"Welcome"}
+
+text_posts={}
+
+@app.get("/posts")
+def get_all_posts(limit:int=None):
+    if limit is not None:
+        return text_posts[:limit]
+    return text_posts
+
+@app.get("/posts/{id}")
+def get_post(id:int):
+    if id not in text_posts: raise HTTPException(status_code=404,detail="Post not found")
+
+    return text_posts.get(id)
+
+@app.post("/posts")
+def create_post(post:PostCreate):
+    id=len(text_posts)+1
+    new_post={"title":post.title,"content":post.content}
+
+    text_posts[id]=new_post
+    
+    return new_post
