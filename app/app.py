f7b2811b004225a0136527ec631a7551690a3bca
Hash-D25 <harshita2386@gmail.com>
2026-07-03 19:33:36 +0530

    .

--- app/app.py
+++ app/app.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI,HTTPException
-from app.schemas import PostCreate
+from app.schemas import PostCreate,PostResponse
 
 app=FastAPI()
 
@@ -7,22 +7,22 @@ app=FastAPI()
 text_posts={}
 
 @app.get("/posts")
-def get_all_posts(limit:int=None):
+def get_all_posts(limit:int):
     if limit is not None:
         return text_posts[:limit]
     return text_posts
 
 @app.get("/posts/{id}")
-def get_post(id:int):
+def get_post(id:int)->PostResponse:
     if id not in text_posts: raise HTTPException(status_code=404,detail="Post not found")
 
     return text_posts.get(id)
 
 @app.post("/posts")
-def create_post(post:PostCreate):
+def create_post(post:PostCreate)->PostResponse:
     id=len(text_posts)+1
     new_post={"title":post.title,"content":post.content}
 
     text_posts[id]=new_post
-    
+
     return new_post
